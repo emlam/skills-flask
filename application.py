69384a1e2db9388061a40ce3292f43ef8f22f7e4
Your Name <you@example.com>
2016-01-24 12:42:41 -0800

    changes

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -13,19 +13,20 @@ def application_page():
     #
     # return render_template("index.html")
 
-@app.route('/application-response',methods=["GET"])
+@app.route('/application-response',methods=["POST"])
 def application_response():
     """Shows the application response page form."""
 
     input_fname = request.args.get("firstname")
+    input_lname = request.args.get("lastname")
+    input_jobreq = request.args.get("jobrequirement")
+    input_salaryreq = request.args.get("salaryrequirement")
 
     return render_template("application-response.html",
-                            input_fname=firstname)
-
-#     Thank you, Jessica McHackbright, for applying to be a QA Engineer. Your
-# minimum salary requirement is 89000 dollars.
-
-
+                        firstname=input_fname,
+                        lastname=input_lname,
+                        jobrequirement=input_jobreq,
+                        salaryrequirement=input_salaryreq)
 
 
 if __name__ == "__main__":
